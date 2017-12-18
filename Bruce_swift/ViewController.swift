c5e5bdf258cb3c5ac78526dfce40c4a9a94358d1
lxllzh <liuzhi@eqxiu.com>
2017-12-18 23:47:24 +0800

    upload project

---
+++ Bruce_swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Bruce_swift
+//
+//  Created by lxllzh on 2017/12/18.
+//  Copyright © 2017年 lxllzh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.orange;
+        NSLog("test------");
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+
+}
+
