cf17274e0cc1b5bbfb5680edd414849352328778
lxllzh <liuzhi@eqxiu.com>
2017-12-21 17:08:32 +0800

    pod update

--- Bruce_swift/ViewController.swift
+++ Bruce_swift/ViewController.swift
@@ -8,11 +8,52 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController ,UITableViewDelegate,UITableViewDataSource {
+    
+    var table :UITableView!
+    var ctrNames:[String]?
+    let arr:[String] = ["w","s"]
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 100
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = (table.dequeueReusableCell(withIdentifier: "cellID", for: indexPath)) as UITableViewCell
+        NSLog("********--cellForRowAt--cell--%d--",indexPath.row);
 
+        return cell
+    }
+    
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        
+        NSLog("********--heightForRow--高度--indexPath-%d--",indexPath.row);
+
+        return 50
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = UIColor.orange;
+        //self.view.backgroundColor = UIColor.orange;
+        
+        let rect = self.view.frame;
+        table = UITableView(frame:rect)
+        self.table.backgroundColor = UIColor.blue
+        self.view.addSubview(table!)
+        //设置数据源
+        self.table.dataSource = self
+        //设置代理
+        self.table.delegate = self
+        self.view.addSubview(table)
+        //注册UITableView，cellID为重复使用cell的Identifier
+        self.table.register(UITableViewCell.self, forCellReuseIdentifier: "cellID")
+        
         NSLog("********--test---*******");
     }
 
@@ -20,6 +61,8 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
     }
 
+    
+    
 
 }
 
---
+++ Podfile
@@ -0,0 +1,35 @@
+platform :ios, '8.0'
+use_frameworks!
+target ‘Bruce_swift’ do
+
+
+pod 'AFNetworking', '~> 3.0'
+pod 'SDWebImage', '~> 4.1'
+pod 'MagicWindowSDK', '~> 3.90.161205’
+pod 'JPush'
+pod 'MJExtension', '~> 3.0'
+
+pod 'MJRefresh', '~> 3.1'
+pod 'CocoaLumberjack'
+pod 'ReactiveCocoa', '~> 2.5'
+pod 'MBProgressHUD', '~> 0.9'
+pod 'SSKeychain', '~> 1.2.2'
+pod 'IQKeyboardManager', '~> 3.2.0.3'
+pod 'SVPullToRefresh', '~> 0.4.1'
+pod 'MKNetworkKit', '~> 0.87'
+pod 'WebViewJavascriptBridge', '~> 4.1.4'
+pod 'FormatterKit', '~> 1.8.0'
+pod 'DZNEmptyDataSet', '~> 1.5.1'
+pod 'Ono', '~> 1.2.0'
+pod 'FMDB'
+pod 'GPUImage', '~> 0.1.7'
+pod 'Reveal-iOS-SDK', '~> 1.6.0'
+pod 'Appirater'
+pod 'SDVersion'
+pod 'YYKit'
+
+
+
+pod 'Moya/ReactiveSwift'
+pod 'Alamofire', '~> 4.5'
+end
